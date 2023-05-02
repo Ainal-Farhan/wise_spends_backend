4570674761aea58a1abaf480b763a54a21b67682
Ainal-Farhan <mohd.ainalfarhan7@gmail.com>
2023-05-02 19:44:25 +0800

    Initial Setup

---
+++ src/main/java/com/ainal/apps/wise_spends/WiseSpendsApplication.java
@@ -0,0 +1,13 @@
+package com.ainal.apps.wise_spends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseSpendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiseSpendsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ainal/apps/wise_spends/WiseSpendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ainal.apps.wise_spends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseSpendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
