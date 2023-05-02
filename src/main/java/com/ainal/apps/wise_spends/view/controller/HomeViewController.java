62d6b5e46617f8fa5914cbbfd9c694bb6c6b2ad8
Ainal-Farhan <mohd.ainalfarhan7@gmail.com>
2023-05-02 22:39:06 +0800

    Initial Setup

---
+++ src/main/java/com/ainal/apps/wise_spends/view/controller/HomeViewController.java
@@ -0,0 +1,12 @@
+package com.ainal.apps.wise_spends.view.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeViewController {
+	@GetMapping("/home")
+	public String home() {
+		return "/public/home";
+	}
+}
