6e146c4f814c5c7e166fd6e51573dc77f7bfa85b
Mohd Ainal Farhan Mohamad Johari <56626447+Ainal-Farhan@users.noreply.github.com>
2023-06-26 10:51:10 +0800

    Update SecurityConfiguration.java

--- src/main/java/com/ainal/apps/wise_spends/security/config/SecurityConfiguration.java
+++ src/main/java/com/ainal/apps/wise_spends/security/config/SecurityConfiguration.java
@@ -54,7 +54,7 @@ public class SecurityConfiguration {
 
 	@Bean
 	AuthenticationEntryPoint authenticationEntryPoint() {
-		return new LoginUrlAuthenticationEntryPoint("/auth/login");
+		return new LoginUrlAuthenticationEntryPoint("/login/auth");
 	}
 
 	@Bean
